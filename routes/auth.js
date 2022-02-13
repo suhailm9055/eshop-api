79c0f39e4c30de166f53ccd3f0dd6b962ee878fa
suhaiLap-hp <suhailm9055@gmail.com>
2022-02-13 20:35:29 +0530

    password hashing with crypto-js

--- routes/auth.js
+++ routes/auth.js
@@ -2,25 +2,26 @@ const User = require("../models/User");
 
 const router = require("express").Router();
 
-
+const CryptoJS = require("crypto-js");
 
 //register
 
-router.post("/register", async (req,res)=>{
-    const newUser= new User({
-        username:req.body.username,
-        email:req.body.email,
-        password:req.body.password,
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SECRT
+    ).toString(),
+  });
 
-    })
-
-    try{
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser)
-
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
