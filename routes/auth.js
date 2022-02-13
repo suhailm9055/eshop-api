03d1b0c7ebedf35e9a9df6e9776f4828127b3ec2
suhaiLap-hp <suhailm9055@gmail.com>
2022-02-13 21:20:48 +0530

    login router

--- routes/auth.js
+++ routes/auth.js
@@ -24,4 +24,21 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//login
+
+router.post("/login", async (req,res)=>{
+    try{
+        const user =await User.findOne({username:req.body.username});
+        !user && res.status(401).json("User not found")
+
+        const hashedPassword= CryptoJS.AES.decrypt(user.password,process.env.PASS_SECRT)
+        const OGpassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+        OGpassword !== req.body.password && res.status(401).json("Wrong Password!")
+        const{password,...other} =user._doc;
+        res.status(200).json(other)
+    }catch(err){
+        res.status(500).json(err)
+    } 
+})
+
 module.exports = router;
