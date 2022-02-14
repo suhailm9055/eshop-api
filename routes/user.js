6cfc756c3b39ed3661daaae9993a0da375affde1
suhaiLap-hp <suhailm9055@gmail.com>
2022-02-14 17:12:48 +0530

    user router update

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,45 @@
+const User = require("../models/User");
+const { varifyTokenAndAuthorization, varifyTokenAndAdmin } = require("./verifyToken");
+
 const router = require("express").Router();
 
 
+//update
+router.put("/:id", varifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SECRT
+    ).toString();
+  }
+  try{
+      const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+        $set :req.body  
+    },{new:true})
+    const {password,...other} = updatedUser._doc;
+    res.status(200).json(other);  
+  }catch (err){
+      res.status(500).json(err)
+  }
+});
+
+//delete
+router.delete("/:id",varifyTokenAndAdmin,async (req,res)=>{
+    try{
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted.")
+    }catch (err){
+        res.status(500).json(err)
+    }
+})
+//get
+router.delete("/:id",varifyTokenAndAdmin,async (req,res)=>{
+    try{
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted.")
+    }catch (err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router;
---
+++ routes/verifyToken.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+
+const varifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+
+  if (authHeader) {
+      const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_KEY, (err, user) => {
+      if (err) {
+        res.status(403).json("Token is not valid");
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("You are NOT authenticated");
+  }
+};
+
+const varifyTokenAndAdmin = (req, res, next) => {
+  varifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    }else {
+        res.status(403).json("Your are Not authorised do that")
+    }
+  });
+};
+const varifyTokenAndAuthorization = (req, res, next) => {
+  varifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    }else {
+        res.status(403).json("Your are Not authorised do that")
+    }
+  });
+};
+module.exports = { varifyToken,varifyTokenAndAuthorization,varifyTokenAndAdmin };
