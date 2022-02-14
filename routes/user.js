968dbabdf401b8fe3d1232feb423999b1221790d
suhaiLap-hp <suhailm9055@gmail.com>
2022-02-14 20:00:56 +0530

    get userdetails

--- routes/user.js
+++ routes/user.js
@@ -1,45 +1,63 @@
 const User = require("../models/User");
-const { varifyTokenAndAuthorization, varifyTokenAndAdmin } = require("./verifyToken");
+const {
+  varifyTokenAndAuthorization,
+  varifyTokenAndAdmin,
+} = require("./verifyToken");
 
 const router = require("express").Router();
 
-
 //update
-router.put("/:id", varifyTokenAndAuthorization, async (req, res) => {
+router.put("/update/:id", varifyTokenAndAuthorization, async (req, res) => {
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
       req.body.password,
       process.env.PASS_SECRT
     ).toString();
   }
-  try{
-      const updatedUser = await User.findByIdAndUpdate(req.params.id,{
-        $set :req.body  
-    },{new:true})
-    const {password,...other} = updatedUser._doc;
-    res.status(200).json(other);  
-  }catch (err){
-      res.status(500).json(err)
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    const { password, ...other } = updatedUser._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
 //delete
-router.delete("/:id",varifyTokenAndAdmin,async (req,res)=>{
-    try{
-        await User.findByIdAndDelete(req.params.id)
-        res.status(200).json("User has been deleted.")
-    }catch (err){
-        res.status(500).json(err)
-    }
-})
+router.delete("/delete/:id", varifyTokenAndAdmin, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted.");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //get
-router.delete("/:id",varifyTokenAndAdmin,async (req,res)=>{
-    try{
-        await User.findByIdAndDelete(req.params.id)
-        res.status(200).json("User has been deleted.")
-    }catch (err){
-        res.status(500).json(err)
-    }
-})
+router.get("/userdetails/:id", varifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//getAllUsers
+router.get("/allusers", varifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
